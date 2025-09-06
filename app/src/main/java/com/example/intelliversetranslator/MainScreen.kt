59bd6dd74e465de4b54005962a568c1871ea0acd
natalymerenbach <nataly@sureuniversal.com>
2025-09-07 02:05:43 +0300

    Fixes - move model to correct package

--- app/src/main/java/com/example/intelliversetranslator/MainScreen.kt
+++ app/src/main/java/com/example/intelliversetranslator/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import model.MyViewModel
 import util.Language
 
 @Composable
